ad8a8ab501d79d9ced7acebc9808b185ae8c724b
romerolukakuelgoat-prog <romerolukakuelgoat@gmail.com>
2026-07-22 17:40:40 -0500

    Update calculadora.py
    Agregar la resta 
    renato lazaro

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,21 @@
-## CALCULADORA - VERSIÓN 1
-# Autor(a): NOMBRE APELLIDO
+## CALCULADORA - VERSIÓN 2
+# Autor(a): Renato Lazaro
 # ===========================================
+print("Calculadora")
+
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-suma = num1 + num2
+print("\n 1.Sumar")
+print("2. Restar")
 
-print("Resultado:", suma)
+opcion = input("\n Seleccione una opcion:")
+
+if opcion =="1":
+  print("\ La suma es:", num1 + num2)
+
+elif opcion == "2":
+  print("\n La resta es:", num1 - num2)
+
+else:
+  print("\n Opcion no valida.")
