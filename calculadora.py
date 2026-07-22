9744a66a88f768621bd01bd59651469c877caa26
romerolukakuelgoat-prog <romerolukakuelgoat@gmail.com>
2026-07-22 17:50:33 -0500

    Update calculadora.py
    Agregar multiplicacion 
    renato lazaro

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-## CALCULADORA - VERSIÓN 2
+## CALCULADORA - VERSIÓN 3
 # Autor(a): Renato Lazaro
 # ===========================================
 print("Calculadora")
@@ -8,7 +8,7 @@ num2 = float(input("Ingrese el segundo número: "))
 
 print("\n 1.Sumar")
 print("2. Restar")
-
+print("3. Multiplicar")
 opcion = input("\n Seleccione una opcion:")
 
 if opcion =="1":
@@ -16,6 +16,9 @@ if opcion =="1":
 
 elif opcion == "2":
   print("\n La resta es:", num1 - num2)
+
+else: opcion == "3":  
+  print("\n La multiplicacion es:",num1 * num2)
 
 else:
   print("\n Opcion no valida.")
