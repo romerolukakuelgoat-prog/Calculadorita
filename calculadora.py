f3938676bc446a30277e7ac202efed82af4881d9
romerolukakuelgoat-prog <romerolukakuelgoat@gmail.com>
2026-07-22 17:55:15 -0500

    Update calculadora.py
    Agregar division

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-## CALCULADORA - VERSIÓN 3
+## CALCULADORA - VERSIÓN 4 
 # Autor(a): Renato Lazaro
 # ===========================================
 print("Calculadora")
@@ -9,6 +9,7 @@ num2 = float(input("Ingrese el segundo número: "))
 print("\n 1.Sumar")
 print("2. Restar")
 print("3. Multiplicar")
+print("4. Dividir")
 opcion = input("\n Seleccione una opcion:")
 
 if opcion =="1":
@@ -19,6 +20,12 @@ elif opcion == "2":
 
 else: opcion == "3":  
   print("\n La multiplicacion es:",num1 * num2)
+
+elif opcion =="4":
+  if num2 !=0:
+    print("\n La division es:" , num1/num2)
+  else:
+    print("No se puede dividir entre cero.")
 
 else:
   print("\n Opcion no valida.")
